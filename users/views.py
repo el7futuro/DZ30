eed2f88968d79c82288e81c3f56092b82ef46422
el7futuro <el7futuro@gmail.com>
2023-01-10 23:24:06 +0500

    1.04

---
+++ users/views.py
@@ -0,0 +1,38 @@
+from django.db.models import Count
+from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+
+from users.models import User, Location
+from users.serializers import UserDetailSerializer, UserListSerializer, UserCreateSerializer, UserUpdateSerializer, \
+    LocationSerializer
+
+
+class UserView(ListAPIView):
+    queryset = User.objects.prefetch_related('location').filter(ads__is_published=True).annotate(total_ads=Count('ads')).order_by('username')
+
+    serializer_class = UserListSerializer
+
+
+class UserDetail(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+
+
+class UserCreateView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+
+
+class UserUpdateView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+
+
+class UserDeleteView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+
+
+class LocationViewSet(ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
